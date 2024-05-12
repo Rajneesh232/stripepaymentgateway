a5d4896ad33ae3ef09265ece7c310c818addd886
Rajneesh232 <rajneeshpal232@gmail.com>
2024-05-12 16:41:49 +0530

    initial commit

---
+++ src/main/java/com/payment/StripePaymentApplication.java
@@ -0,0 +1,26 @@
+package com.payment;
+
+import com.payment.controller.PaymentController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class StripePaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StripePaymentApplication.class, args);
+	}
+
+
+	@Value("${stripe.secretKey}")
+	private String secretKey;
+
+	@Bean
+	public PaymentController paymentController() {
+		return new PaymentController(secretKey);
+	}
+
+}
+
---
+++ src/main/java/com/payment/controller/PaymentController.java
@@ -0,0 +1,43 @@
+package com.payment.controller;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.payment.payload.PaymentRequest;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import com.stripe.param.PaymentIntentCreateParams;
+
+@RestController
+@RequestMapping("/api/payment")
+public class PaymentController {
+
+    private final String secretKey;
+
+    public PaymentController(@Value("${stripe.secretKey}") String secretKey) {
+        this.secretKey = secretKey;
+        Stripe.apiKey = secretKey;
+    }
+
+    @PostMapping("/create-payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentRequest paymentRequest) {
+        try {
+            PaymentIntentCreateParams createParams = new PaymentIntentCreateParams.Builder()
+                    .setAmount(paymentRequest.getAmount() * 100) // Convert amount to cents
+                    .setCurrency(paymentRequest.getCurrency())
+                    .build();
+
+            PaymentIntent paymentIntent = PaymentIntent.create(createParams);
+
+            return ResponseEntity.ok("PaymentIntent created successfully with ID: " + paymentIntent.getId());
+        } catch (StripeException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error creating PaymentIntent: " + e.getMessage());
+        }
+    }
+}
